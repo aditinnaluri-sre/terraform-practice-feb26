bc0f818e44cfcb4a3b5da63fe1a72ec78cf7e4c5
Adi Tinnaluri <tans.trainings@gmail.com>
2026-06-28 00:13:22 +0100

    Change storage bucket location to europe-west3

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "buk-dev-tans-trainings" {
   name     = "bkt-demo-0101-tans-1"
-  location = "asia-south1"
+  location = "europe-west3"
   project  = "terraform-gcp-498312"
   force_destroy = true
   public_access_prevention = "enforced"
