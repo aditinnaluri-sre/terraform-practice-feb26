f2211b4c74d5e9a403c2d557277d599b36f3579a
Adi Tinnaluri <tans.trainings@gmail.com>
2026-06-28 00:26:04 +0100

    Rename Google Storage Bucket for clarity

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "my-bucket" {
-  name     = "bkt-tfstate-00-tans"
+  name     = "bk-terraform-practice-feb26-280626"
   location = "europe-west3"
   project  = "terraform-gcp-498312"
   force_destroy = true
